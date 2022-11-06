e432c3a521db8005623adf7981f31bd8d56ad18f
kelly ikenga <ikengakelly446@gmail.com>
2022-11-06 16:06:00 +0100

    Authentication

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,25 +1,14 @@
 import { async } from '@firebase/util';
 import React from 'react'
 import { FcGoogle } from "react-icons/fc";
-import { UserAuth } from '../context/AuthContext';
+
 
 export const  Signup = () => {
-  const {goggleSignIn}=UserAuth()
-
-  const handleGoogleSignin=async()=>{
-    try{
-      await goggleSignIn
-      
-
-    }
-    catch (error){
-      console.log(error)
-    }
-  }
+ 
   return (
     <div className="sign"> 
         <h2 className='underline'>Signup</h2>
-        <button onClick={handleGoogleSignin} className="btn-cont" > <FcGoogle/> sign</button>
+        <button  className="btn-cont" > <FcGoogle/> sign</button>
 
     </div>
   )
